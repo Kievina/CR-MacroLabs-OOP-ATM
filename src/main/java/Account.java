ff9fe8d87743aeefe4b09f2620a312de30d9c8cb
Kievina <56592501+Kievina@users.noreply.github.com>
2019-10-29 13:43:29 -0400

    add methods for deposit, withdrawal and transfer

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,14 +1,42 @@
 public abstract class Account {
 
     private Integer accountNum;
-    private Double accountBalance;
+    private Double balance;
     private char accountType;
 
+    public Account() {}
     public Account(int accountNum) {
         this.accountNum = accountNum;
     }
 
-    public void setAccountNum() {
-        this.accountNum = userBank.returnNewAccountNum;
+    public void setBalance(Double balance) {
+        this.balance = balance;
     }
+    public Double getBalance() { return balance; }
+
+
+    public void deposit(Double depositAmount) {
+        Double balance = this.getBalance();
+        balance = balance + depositAmount;
+        this.setBalance(balance);
+    }
+
+
+    public void withdraw(Double withdrawalAmount) {
+        Double balance = this.getBalance();
+        balance = balance - withdrawalAmount;
+        this.setBalance(balance);
+    }
+
+    //Transfer money between account types
+    public void transfer(Account accountType, Double transferAmount)
+    {
+        this.setBalance(getBalance() - transferAmount);
+        accountType.setBalance(accountType.getBalance() + transferAmount);
+    }
+
+    /*public String printTransactionHistory() {
+        return "Here is your most recent transaction: ""
+
+    }*/
 }
--- src/main/java/Checking.java
+++ src/main/java/Checking.java
@@ -1,2 +1,5 @@
 public class Checking extends Account {
+    public Checking() {
+
+    }
 }
